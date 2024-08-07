2020598c304fab9922006567922fece577a273ba
senobela <145525514+senomaster@users.noreply.github.com>
2024-08-07 11:24:09 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUGAS</title>
+</head>
+<body>
+<Center>
+<?php
+
+echo"<h1>SELAMAT DI KELAS XII RPL B </h1>";
+
+?>
+</body>
+</html>
